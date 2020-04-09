f586b662a12d5f0cbe7669692330b98032bcb6a3
Rawdan01 <63416448+Rawdan01@users.noreply.github.com>
2020-04-09 18:51:54 +0200

    Create Personne.cs

---
+++ Personne.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.E.R.M
+{
+    class Personne
+    {
+        private int Id;
+        private string Nom_Complet;
+        private string Type;
+        public int id
+        {
+            get => id;
+            set => id = value; 
+        }
+        public string nom_Complet
+        {
+            get => nom_Complet;
+            set => nom_Complet = value;
+        }
+       
+        public Personne (int Id,string Nom_complet)
+        {
+            this.id = Id;
+            this.nom_Complet = Nom_complet;
+           
+
+        }
+        public string afficher()
+        {
+            return this.id + "  " + this.nom_Complet ;
+        }
+    }
+}
